b92eb544e83d63d63613605a56c009865d351a57
장한별 <hanquf@rsquare.co.kr>
2023-10-20 19:41:20 +0900

    환경변수 작업중

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_NAVER_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
