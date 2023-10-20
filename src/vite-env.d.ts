e99935d7c113027df09dd975b8a8137a3802aacc
장한별 <hanquf@rsquare.co.kr>
2023-10-20 19:52:52 +0900

    작업중

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,1 @@
 /// <reference types="vite/client" />
-interface ImportMetaEnv {
-  readonly VITE_APP_TITLE: string;
-  readonly VITE_NAVER_KEY: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
